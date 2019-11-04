ccc00ca30d9342dba7cb6c003394bd9acfe761d7
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-11-04 11:25:43 -0600

    finished spring controller exercise

---
+++ src/main/java/com/codeup/blog/blog/controllers/PostController.java
@@ -0,0 +1,36 @@
+package com.codeup.blog.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String index(){
+        return "posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String viewIndividualPost(@PathVariable String id){
+        return "view an individual post for id of " + id;
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String viewCreateForm(){
+        return "view the form for creating a post";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String createPost(){
+        return "create a new post";
+    }
+
+}

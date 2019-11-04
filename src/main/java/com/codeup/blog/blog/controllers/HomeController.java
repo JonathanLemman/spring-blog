95a1223d7186dc2eda387652d3456e00b6f5706f
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-11-04 14:03:34 -0600

    took away some spacing

--- src/main/java/com/codeup/blog/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/blog/controllers/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
 
     @GetMapping("/")
     @ResponseBody
-    public String landing(){
+    public String index(){
         return "This is the landing page!";
     }
 

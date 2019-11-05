03d168925c18e269c25dae5583dd31affb0f4f88
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-11-05 15:00:14 -0600

    finished exercise now to work on css

--- src/main/java/com/codeup/blog/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/blog/controllers/PostController.java
@@ -1,21 +1,30 @@
 package com.codeup.blog.blog.controllers;
 
+import com.codeup.blog.blog.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String index(){
-        return "posts index page";
+    public String index(Model vModel){
+        ArrayList<Post> postList = new ArrayList<>();
+        postList.add(new Post(4, "Test Title 1", "Test body 1"));
+        postList.add(new Post(5, "Test Title 2", "Test body 2"));
+        vModel.addAttribute("posts", postList);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String viewIndividualPost(@PathVariable long id){
-        return "view an individual post for id of " + id;
+    public String viewIndividualPost(@PathVariable long id, Model vModel){
+        Post newPost = new Post(id, "The Green Tree", "I have no clue what the title is all about!");
+        vModel.addAttribute("post", newPost);
+//        vModel.addAttribute("id", id);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")

cf11fa526df7f920cd97b75f9f51ab0b339dc155
Jonathan Lemman <jonathan.lemman@gmail.com>
2019-11-06 16:26:56 -0600

    working on exercise

--- src/main/java/com/codeup/blog/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/blog/controllers/PostController.java
@@ -1,6 +1,7 @@
 package com.codeup.blog.blog.controllers;
 
-import com.codeup.blog.blog.Post;
+import com.codeup.blog.blog.models.Post;
+import com.codeup.blog.blog.repository.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,25 +11,21 @@ import java.util.ArrayList;
 @Controller
 public class PostController {
 
-    ArrayList<Post> postList;
+    private final PostRepository postDao;
 
-    public PostController() {
-        postList = new ArrayList<>();
-
-        postList.add(new Post(1, "Test Title 1", "Test body 1"));
-        postList.add(new Post(2, "Test Title 2", "Test body 2"));
-        postList.add(new Post(3, "Test Title 3", "Test body 3"));
+    public PostController(PostRepository postDao) {
+        this.postDao = postDao;
     }
 
     @GetMapping("/posts")
     public String index(Model vModel){
-        vModel.addAttribute("posts", postList);
+        vModel.addAttribute("posts", postDao.findAll());
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
     public String viewIndividualPost(@PathVariable long id, Model vModel){
-        vModel.addAttribute("post", postList.get((int)id-1));
+        vModel.addAttribute("post", postDao.getOne(id));
         return "posts/show";
     }
 
@@ -43,5 +40,39 @@ public class PostController {
     public String createPost(@RequestParam String title, @RequestParam String body){
         return "create a new post";
     }
+
+
+
+    @GetMapping("/posts/edit")
+    public String editForm(){
+        return "posts/edit";
+    }
+
+    @PostMapping("/posts/edit")
+    public String editPost(@PathVariable long id, @RequestParam(name = "title") String title, @RequestParam(name = "body") String body){
+        return "redirect:/posts";
+    }
+
+
+
+//    @PostMapping("/posts/delete")
+//    public String deletePost(@RequestParam(name = "post") long id){
+//        postDao.deleteById(id);
+//        return "redirect:/posts";
+//    }
+
+    @PostMapping("/posts/{id}/delete")
+    public String deletePost(@PathVariable long id){
+        postDao.deleteById(id);
+        return "redirect:/posts";
+    }
+
+    /* It updates model object. */
+//    @RequestMapping(value="/editsave",method = RequestMethod.POST)
+//    public String editsave(@ModelAttribute("emp") Emp emp){
+//        dao.update(emp);
+//        return "redirect:/viewemp";
+//    }
+
 
 }
--- src/main/java/com/codeup/blog/blog/models/Post.java
+++ src/main/java/com/codeup/blog/blog/models/Post.java
@@ -1,9 +1,20 @@
-package com.codeup.blog.blog;
+package com.codeup.blog.blog.models;
 
+import javax.annotation.processing.Generated;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String body;
 
     public Post(long id, String title, String body) {
@@ -11,6 +22,8 @@ public class Post {
         this.title = title;
         this.body = body;
     }
+
+    public Post(){}
 
     public long getId() {
         return id;
